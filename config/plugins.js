be62b1562ec91a76b43f8e78d2e9fafbabdba336
Charles Emmanuel <relate2hazel@gmail.com>
2023-09-07 17:50:24 +0100

    Strapi Fix -!

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,17 @@
 module.exports = ({ env }) => ({
   upload: {
-    config:{
+    config: {
       provider: 'cloudinary',
       providerOptions: {
+        cloud_url: env('CLOUDINARY_URL'),
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET')
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
